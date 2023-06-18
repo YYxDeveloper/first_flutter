811168cbc78b32af9ebeef1e2692b36b11932657
young <yyxdev@gmail.com>
2023-06-18 18:54:26 +0800

    ---OBJECT---3 switch example
    # Conflicts:
    #	lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,118 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+/// Flutter code sample for [Switch].
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
+void main() => runApp(const SwitchApp());
 
-class _MyAppState extends State<MyApp> {
-  bool _switchValue = false;
-  String _displayText = 'Switch is OFF';
+class SwitchApp extends StatelessWidget {
+  const SwitchApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Switch Example'),
-        ),
-        body: Container(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              Text(_displayText),
-              Switch(
-                value: _switchValue,
-                onChanged: (value) {
-                  setState(() {
-                    _switchValue = value;
-                    _displayText = value ? 'Switch is ON' : 'Switch is OFF';
-                  });
-                },
-              ),
-            ],
-          ),
+        appBar: AppBar(title: const Text('Switch Sample')),
+        body: const Center(
+          child: SwitchExample(),
         ),
       ),
+    );
+  }
+}
+
+class SwitchExample extends StatefulWidget {
+  const SwitchExample({super.key});
+
+  @override
+  State<SwitchExample> createState() => _SwitchExampleState();
+}
+
+class _SwitchExampleState extends State<SwitchExample> {
+  bool light = true;
+  bool light0 = true;
+  bool light1 = true;
+  final MaterialStateProperty<Color?> trackColor =
+  MaterialStateProperty.resolveWith<Color?>(
+        (Set<MaterialState> states) {
+      // Track color when the switch is selected.
+      if (states.contains(MaterialState.selected)) {
+        return Colors.amber;
+      }
+      // Otherwise return null to set default track color
+      // for remaining states such as when the switch is
+      // hovered, focused, or disabled.
+      return null;
+    },
+  );
+  final MaterialStateProperty<Color?> overlayColor =
+  MaterialStateProperty.resolveWith<Color?>(
+        (Set<MaterialState> states) {
+      // Material color when switch is selected.
+      if (states.contains(MaterialState.selected)) {
+        return Colors.amber.withOpacity(0.54);
+      }
+      // Material color when switch is disabled.
+      if (states.contains(MaterialState.disabled)) {
+        return Colors.grey.shade400;
+      }
+      // Otherwise return null to set default material color
+      // for remaining states such as when the switch is
+      // hovered, or focused.
+      return null;
+    },
+  );
+  final MaterialStateProperty<Icon?> thumbIcon =
+      MaterialStateProperty.resolveWith<Icon?>(
+    (Set<MaterialState> states) {
+      if (states.contains(MaterialState.selected)) {
+        return const Icon(Icons.check);
+      }
+      return const Icon(Icons.close);
+    },
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Switch(
+          //basic switch
+          // This bool value toggles the switch.
+          value: light,
+          activeColor: Colors.red,
+          onChanged: (bool value) {
+            // This is called when the user toggles the switch.
+            setState(() {
+              light = value;
+
+            });
+          },
+        ),
+        Switch(
+            //  customize Switch using MaterialStateProperty switch properties
+          value: light,
+          overlayColor: overlayColor,
+          trackColor: trackColor,
+          thumbColor: const MaterialStatePropertyAll<Color>(Colors.black),
+          onChanged: (bool value) {
+            // This is called when the user toggles the switch.
+            setState(() {
+              light = value;
+            });
+          },
+        ),
+        Switch(
+          thumbIcon: thumbIcon,
+          value: light1,
+          onChanged: (bool value) {
+            setState(() {
+              light1 = value;
+            });
+          },
+        ),
+      ],
     );
   }
 }
